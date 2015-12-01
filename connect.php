513a4fd8e037160c8e1c8915f98069c9d0b1cc2b
jegladh <jesgla-2@student.ltu.se>
2015-12-01 16:14:00 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,14 +1,8 @@
 <?php
-$servername = "127.0.0.1";
-$username = "root";
-$password = "root";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+$link = mysql_connect('localhost', 'root', 'root');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+mysql_close($link);
 ?>
