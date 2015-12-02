8a494a1ae002a1c4c688eec5d953a2ece1594153
jegladh <jesgla-2@student.ltu.se>
2015-12-02 16:12:01 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,13 +1,1 @@
-<?php
-$servername = "130.240.170.52";
-$username = "root";
-$password = "root";
-$dbname = "catcher";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-?>
