4f9c9f54310700b3de7b4c1fce5dd0d3fa74ed1f
jegladh <jesgla-2@student.ltu.se>
2015-12-03 08:58:20 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 
 
 <?php
-// we connect to example.com and port 3307
+// we connect to example.com and port 3306
 $link = mysql_connect('cloud-52.skelabb.ltu.se:3306', 'root', 'root');
 if (!$link) {
     die('Could not connect: ' . mysql_error());
@@ -9,7 +9,7 @@ if (!$link) {
 echo 'Connected successfully';
 mysql_close($link);
 
-// we connect to localhost at port 3307
+// we connect to localhost at port 3306
 $link = mysql_connect('127.0.0.1:3306', 'root', 'root');
 if (!$link) {
     die('Could not connect: ' . mysql_error());
