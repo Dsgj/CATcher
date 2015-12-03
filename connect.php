10959fde3cb72d740aee925c5b80b5c66532e608
jegladh <jesgla-2@student.ltu.se>
2015-12-03 08:46:53 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,9 +2,10 @@
 $username = "root";
 $password = "root";
 $hostname = "127.0.0.1"; 
+$port = "3306";
 
 //connection to the database
-$dbhandle = mysql_connect($hostname, $username, $password) 
+$dbhandle = mysql_connect($hostname. ':' .$port, $username, $password) 
   or die("Unable to connect to MySQL");
 echo "Connected to MySQL<br>";
 ?>
