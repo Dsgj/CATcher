945211132833109247af24877d9b552da3f6ea34
Leo Ozolins Carlson <leocar-2@student.ltu.se>
2015-12-11 14:23:07 +0100

    FuckGit

--- connect.php
+++ connect.php
@@ -1,7 +1,9 @@
 <?php
+
 $servername = "127.0.0.1";
 $username = "root";
 $password = "root";
+
 try {
     $conn = new PDO("mysql:host=$servername;dbname=catcher", $username, $password);
     // set the PDO error mode to exception
@@ -12,8 +14,4 @@ catch(PDOException $e)
 {
     echo "Connection failed: " . $e->getMessage();
 }
-<<<<<<< HEAD
 ?>
-=======
-?>
->>>>>>> ea8d5ab555c2085660dc0484fb4f3ab6f388c788
