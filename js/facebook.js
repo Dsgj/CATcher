faead963139ff71ba6c6f837e6c2dc461b124a06
jegladh <jesgla-2@student.ltu.se>
2015-12-14 15:38:16 +0100

    Update facebook.js

--- js/facebook.js
+++ js/facebook.js
@@ -55,7 +55,7 @@ window.fbAsyncInit = function () {
 // successful.  See statusChangeCallback() for when this call is made.
 function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me?scope=email', function (response) {
+    FB.api('/me', function (response) {
         console.log('Successful login for: ' + response.name);
         document.getElementById('status').innerHTML =
             'Thanks for logging in, ' + response.name + '!';
