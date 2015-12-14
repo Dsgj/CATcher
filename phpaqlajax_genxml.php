e439409299d45ef409e165138f188696b425e3b7
Leo Ozolins Carlson <leocar-2@student.ltu.se>
2015-12-14 11:35:06 +0100

    pushing

--- phpaqlajax_genxml.php
+++ phpaqlajax_genxml.php
@@ -17,8 +17,8 @@ if (!$connection) {
     die('Not connected : ' . mysql_error());
 }
 
-// Set the active MySQL database
-$db_selected = mysql_select_db($database, $connection);
+// Set the active MySQL dbname
+$db_selected = mysql_select_db($dbname, $connection);
 if (!$db_selected) {
     die ('Can\'t use db : ' . mysql_error());
 }
