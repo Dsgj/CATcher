a2f93b2c27a2a4f99de0a671f6c553a1ad0b0683
Leo Ozolins Carlson <leocar-2@student.ltu.se>
2015-12-14 22:01:16 +0100

    livetärmisär

--- header.php
+++ header.php
@@ -40,7 +40,73 @@
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 
     <![endif]-->
+    <script src="js/jquery.js">
+        var customIcons = {
+            male: {
+                icon: 'http://labs.google.com/ridefinder/images/mm_20_blue.png'
+            },
+            female: {
+                icon: 'http://labs.google.com/ridefinder/images/mm_20_red.png'
+            }
+        };
 
+        function load() {
+            var map = new google.maps.Map(document.getElementById("map"), {
+                center: new google.maps.LatLng(65.6177455, 22.137957),
+                zoom: 13,
+                mapTypeId: 'roadmap'
+            });
+            var infoWindow = new google.maps.InfoWindow;
+
+            downloadUrl("phpsqlajax_genxml.php", function (data) {
+                var xml = data.responseXML;
+                var markers = xml.documentElement.getElementsByTagName("marker");
+                for (var i = 0; i < markers.length; i++) {
+                    var name = markers[i].getAttribute("name");
+                    var address = markers[i].getAttribute("address");
+                    var sex = markers[i].getAttribute("sex");
+                    var point = new google.maps.LatLng(
+                        parseFloat(markers[i].getAttribute("lat")),
+                        parseFloat(markers[i].getAttribute("lng")));
+                    var html = "<b>" + name + "</b> <br/>" + address;
+                    var icon = customIcons[sex] || {};
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        position: point,
+                        icon: icon.icon
+                    });
+                    bindInfoWindow(marker, map, infoWindow, html);
+                }
+            });
+        }
+
+        function bindInfoWindow(marker, map, infoWindow, html) {
+            google.maps.event.addListener(marker, 'click', function () {
+                infoWindow.setContent(html);
+                infoWindow.open(map, marker);
+            });
+        }
+
+        function downloadUrl(url, callback) {
+            var request = window.ActiveXObject ?
+                new ActiveXObject('Microsoft.XMLHTTP') :
+                new XMLHttpRequest;
+
+            request.onreadystatechange = function () {
+                if (request.readyState == 4) {
+                    request.onreadystatechange = doNothing;
+                    callback(request, request.status);
+                }
+            };
+
+            request.open('GET', url, true);
+            request.send(null);
+        }
+
+        function doNothing() {
+        }
+        $(function() { load(); });
+    </script>
 </head>
 <body>
 <script src="js/facebook.js"></script>
