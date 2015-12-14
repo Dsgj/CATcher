7e44d267a56b3348ad00be1b025a55e7cf8928d9
jegladh <jesgla-2@student.ltu.se>
2015-12-14 11:03:22 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -11,6 +11,7 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    mysql_set_charset('utf8',$conn);
     echo "Connected successfully";
 
 
